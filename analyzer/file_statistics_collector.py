162ed2ebfe3e28f65e69e0f0c0757e55c37f5102
elkaboussi <elkaboussi@pm.me>
2024-02-27 23:12:57 +0100

    refactor: file_statistics_collector

--- analyzer/file_statistics_collector.py
+++ analyzer/file_statistics_collector.py
@@ -1,58 +1,49 @@
-import sys
 import time
+from os import stat
 from pathlib import Path
+from typing import Union
 
-from bitmath import Byte
-from rich.console import Console
+import bitmath
+from rich import print
 
 
 class FileStatisticsCollector:
-    def __init__(self):
-        self.total_files = 0
-        self.total_size = 0
-        self.average_size = 0
-        self.smallest_file_size = sys.maxsize
-        self.largest_file_size = 0
-        self.start_time = time.time()
+    total_files: int = 0
+    total_size: int = 0
+    average_size: float = 0.0
+    smallest_file_size: float = float("inf")
+    largest_file_size: float = 0.0
+    start_time: float = time.time()
+    report_key_len: int = len("Smallest File Size:   ")
 
     def add_file(self, file_path: Path) -> None:
         try:
-            file_size = file_path.stat().st_size
+            file_size = stat(file_path).st_size
         except (FileNotFoundError, OSError):
             return
 
         self.total_files += 1
         self.total_size += file_size
-        self.average_size = self.total_size / self.total_files
 
         self.smallest_file_size = min(self.smallest_file_size, file_size)
         self.largest_file_size = max(self.largest_file_size, file_size)
 
+    def _format_size_line(self, key: str, value: Union[int, float]) -> str:
+        formatted_value = bitmath.Byte(value).best_prefix(bitmath.SI)
+        return f"{key.ljust(self.report_key_len)} {formatted_value}"
+
     def report_statistics(self) -> None:
         end_time = time.time()
         elapsed_time = end_time - self.start_time
+        if self.total_files == 0:
+            return
+        self.average_size = self.total_size / self.total_files
+        bitmath.format_string = "{value:.2f} {unit}"
 
-        indent_length = len("Smallest File Size:   ")
-        console = Console()
-        console.print("[bold green]File Statistics Report[/bold green]")
-        console.print("=" * 23)
-        console.print("Total Files:".ljust(indent_length) + f"{self.total_files}")
-        console.print(
-            "Total Size:".ljust(indent_length)
-            + f"{Byte(self.total_size).to_Byte().best_prefix().value:.2f} MB"
-        )
-        console.print(
-            "Average File Size:".ljust(indent_length)
-            + f"{Byte(self.average_size).to_Byte().best_prefix().value:.2f} MB"
-        )
-        console.print(
-            "Smallest File Size:".ljust(indent_length)
-            + f"{Byte(self.smallest_file_size).to_Byte().best_prefix().value:.2f} KB"
-        )
-        console.print(
-            "Largest File Size:".ljust(indent_length)
-            + f"{Byte(self.largest_file_size).to_Byte().best_prefix().value:.2f} MB"
-        )
-        console.print(
-            "Time Elapsed:".ljust(indent_length) + f"{elapsed_time:.2f} seconds"
-        )
+        print("\n[bold][underline]Statistics[/underline][/bold]")
+        print(f"{'Total Files:':<{self.report_key_len}} {self.total_files} file")
+        print(self._format_size_line("Total Size:", self.total_size))
+        print(self._format_size_line("Average File Size:", self.average_size))
+        print(self._format_size_line("Smallest File Size:", self.smallest_file_size))
+        print(self._format_size_line("Largest File Size:", self.largest_file_size))
+        print(f"{'Time Elapsed:':<{self.report_key_len}} {elapsed_time:.2f} seconds")
