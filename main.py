5bc5db41da3345d0c3c885763522009a1d7981d9
elkaboussi <elkaboussi@pm.me>
2024-03-03 20:22:21 +0100

    fix: close log file in all cases

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from analyzer.file_processing import process_directory
 from analyzer.utils.logger import setup_logging
 from analyzer.utils.parser import parse_args
@@ -5,8 +7,8 @@ from analyzer.utils.parser import parse_args
 
 def main():
     arguments = parse_args()
-    if arguments.target_dir is None:
-        exit(1)
+    if not arguments or not arguments.target_dir:
+        sys.exit(1)
 
     setup_logging(
         log_file=arguments.log_file,
@@ -15,12 +17,23 @@ def main():
         delete_files=arguments.delete_files,
     )
 
-    process_directory(
-        dir_path=arguments.target_dir,
-        size_threshold=str(arguments.size_threshold),
-        delete_files=arguments.delete_files,
-        log_file=arguments.log_file,
-    )
+    try:
+        process_directory(
+            dir_path=arguments.target_dir,
+            size_threshold=str(arguments.size_threshold),
+            delete_files=arguments.delete_files,
+            log_file=arguments.log_file,
+        )
+    finally:
+        if arguments.log_file is not None:
+
+            # Close log file
+            sys.stdout.close()
+            sys.stderr.close()
+
+            # Reset back to stanrds
+            sys.stdout = sys.__stdout__
+            sys.stderr = sys.__stderr__
 
 
 if __name__ == "__main__":
